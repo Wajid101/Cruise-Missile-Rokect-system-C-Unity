7f655f49083bf484c0774adca84745a3f6a9f788
Muhammad Wajid <126747412+Wajid101@users.noreply.github.com>
2023-03-29 15:18:14 +0500

    Add files via upload

---
+++ MoverMissile.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class MoverMissile : MonoBehaviour
+{
+    public float Damping = 3;
+    public float Speed = 80;
+    public float SpeedMax = 80;
+    public float SpeedMult = 1;
+    public Vector3 Noise = new Vector3(20, 20, 20);
+    public float TargetLockDirection = 0.5f;
+    public float timeCount = 0;
+    public Rigidbody Obj;
+    public New_Weapon instance;
+
+
+    private void Start()
+    {
+        timeCount = 0;
+        //Destroy (gameObject, LifeTime);
+    }
+
+    private void FixedUpdate()
+    {
+
+        Obj.velocity = new Vector3(transform.forward.x * Speed * Time.fixedDeltaTime, transform.forward.y * Speed * Time.fixedDeltaTime, transform.forward.z * Speed * Time.fixedDeltaTime);
+        Obj.velocity += new Vector3(Random.Range(-Noise.x, Noise.x), Random.Range(-Noise.y, Noise.y), Random.Range(-Noise.z, Noise.z));
+
+        if (Speed < SpeedMax)
+        {
+            Speed += SpeedMult * Time.fixedDeltaTime;
+        }
+    }
+
+    private void Update()
+    {
+        timeCount = timeCount + Time.deltaTime;
+
+        if (instance.closest && timeCount > 3f)
+        {
+            Quaternion rotation = Quaternion.LookRotation(instance.closest.transform.position - transform.transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * Damping);
+            Vector3 dir = (instance.closest.transform.position - transform.position).normalized;
+            float direction = Vector3.Dot(dir, transform.forward);
+            if (direction < TargetLockDirection)
+            {
+                instance.closest = null;
+            }
+        }
+
+       
+    }
+
+}
---
+++ New_Weapon.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class New_Weapon : MonoBehaviour
+{
+    public string[] TargetTag = new string[1] { "E" };
+
+    
+    public Rigidbody Messaile;
+    public List<Rigidbody> Roketlist;
+    public int bcount = 2;
+
+    public GameObject closest;
+    public GameObject[] gos;
+    public GameObject[] gos11;
+    public GameObject[] gos1;
+
+    //public List<GameObject> gos;
+    public float rangeOfEncounter = 11000, dely_time_fire;
+    float Nextfire = 0;
+    public Transform[] barrels;
+    public AudioSource LanchAduio;
+
+
+
+    private void Start()
+    {
+        Roketlist = new List<Rigidbody>();
+        for (int ii = 0; ii < bcount; ii++)
+        {
+            //Debug.Log("call");
+            Rigidbody bomb = (Rigidbody)Instantiate(Messaile);
+            bomb.gameObject.SetActive(false);
+            Roketlist.Add(bomb);
+            Roketlist[ii].GetComponent<MoverMissile>().instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        ClosestObj();
+
+     
+        if (closest != null)
+        {
+            if (Time.time >= Nextfire)
+            {
+                Nextfire = Time.time + dely_time_fire;
+                
+                Fire();
+
+            }
+        }
+
+
+
+    }
+
+
+    GameObject ClosestObj()
+    {
+        if (TargetTag.Length > 1)
+        {
+            gos = GameObject.FindGameObjectsWithTag(TargetTag[0]);
+            gos11 = GameObject.FindGameObjectsWithTag(TargetTag[1]);
+            gos1 = gos.Concat(gos11).ToArray();
+
+        }
+        else
+        {
+            gos = GameObject.FindGameObjectsWithTag(TargetTag[0]);
+            gos1 = gos.ToArray();
+
+        }
+
+
+        closest = null;
+        float distance = rangeOfEncounter;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos1)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+
+       
+
+        return closest;
+
+
+
+    }
+
+
+
+
+
+    public void Fire()
+    {
+
+        for (int i = 0; i < Roketlist.Count; i++)
+        {
+
+
+            if (!Roketlist[i].gameObject.activeInHierarchy)
+            {
+
+                Roketlist[i].transform.position = barrels[i].position;
+                Roketlist[i].transform.rotation = barrels[i].rotation;
+                LanchAduio.Play();
+                Roketlist[i].gameObject.SetActive(true);
+                break;
+
+            }
+        }
+    }
+
+
+}
+
+
